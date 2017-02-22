8c6bed9ee697c5a680c3bd242d63caf37f383227
Zhengming Ding <allanding@ece.neu.edu>
2017-02-22 12:50:34 -0500

    Update objHereInner.m

--- objHereInner.m
+++ objHereInner.m
@@ -1,9 +1,13 @@
 function [ff, ffP]=objHereInner(P,X,Xn,Zn,E,Y1,alpha,mu)
-G0 = P'*X*Zn*X'*P;
-G1 = P'*Xn-E+Y1/mu;
-ff=mu*sum(sum(G1.^2))+alpha*trace(G0);
 
-P1 = 2*alpha*X*Zn*X'+mu*Xn*Xn';
-P2 = Xn*(E-Y1/mu)';
-ffP = P1*P-P2;
+  %% calculate the objective function w.r.t. P
+  G0 = P'*X*Zn*X'*P;
+  G1 = P'*Xn-E+Y1/mu;
+  ff=mu*sum(sum(G1.^2))+alpha*trace(G0);
+
+
+  %% calculate the gradient function w.r.t. P
+  P1 = 2*alpha*X*Zn*X'+mu*Xn*Xn';
+  P2 = Xn*(E-Y1/mu)';
+  ffP = P1*P-P2;
 end
